4d14cb994d60f11c641b37c6e7881035fe80077a
naveenKumarV <naveenkumarvedurupaka@gmail.com>
2015-07-03 19:01:36 +0530

    Create QuestionRequest to handle form validation

---
+++ app/Http/Requests/QuestionRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class QuestionRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'question'          => 'required',
+            'option_A'          => 'required',
+            'option_B'          => 'required',
+            'option_C'          => 'required',
+            'option_D'          => 'required',
+            'answer'            => 'required|alpha|max:1|min:1',
+            'category'          => 'required|in:history,sports,science,literature,politics',
+            'difficulty_rating' => 'required|integer|between:1,4'
+        ];
+    }
+}

b5c60fae906c7658a54f8b6f1a6ef5d21042f0f6
naveenKumarV <naveenkumarvedurupaka@gmail.com>
2015-07-10 01:24:13 +0530

    Add client side validation to login and register views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,17 +2,17 @@
 
 @section('form')
      <div class="panel-heading">
-        <h1>SIGN IN</h1>
+        <h1>LOG IN</h1>
     </div>
     <div class="panel-body">
         {!! Form::open() !!}
         <div class="form-group">
             {!!Form::label('username','Username')!!}
-            {!!Form::text('username', null,array('class' => 'form-control'))!!}
+            {!!Form::text('username', null,array('class' => 'form-control','required' => 'required'))!!}
         </div>
         <div class="form-group">
             {!!Form::label('password','Password')!!}
-            {!!Form::password('password',array('class' => 'form-control'))!!}
+            {!!Form::password('password',array('class' => 'form-control','required' => 'required'))!!}
         </div>
         {!!Form::submit('Login', array('class' => 'btn btn-primary'))!!}
         {!! Form::close() !!}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,22 +10,27 @@
         <div class="form-group">
             {!!Form::label('name','Name')!!}
             {!!Form::text('name', null,array('class' => 'form-control',
-            'placeholder'=>'Your full name'))!!}
+            'placeholder'=>'Your full name','required'=>'required',
+            'pattern'=>"[A-Z,a-z]{3,255}",
+            'title' => "(minimum 3 letters, should not contain numbers and special characters)"))!!}
         </div>
         <div class="form-group">
             {!!Form::label('username','Username')!!}
             {!!Form::text('username', null,array('class' => 'form-control',
-            'placeholder'=>'eg. Ram123'))!!}
+            'placeholder'=>'eg. Ram123','required'=>'required','pattern'=>".{3,255}",
+            'title'=>"(minimum 3 characters, should not contain special characters)"))!!}
         </div>
         <div class="form-group">
             {!!Form::label('password','Password')!!}
             {!!Form::password('password',array('class' => 'form-control',
-            'placeholder'=>'Minimum six characters'))!!}
+            'placeholder'=>'Minimum six characters','required'=>'required',
+            'pattern'=>".{6,50}"))!!}
         </div>
         <div class="form-group">
             {!!Form::label('password_confirmation','Password Confirmation')!!}
             {!!Form::password('password_confirmation',array('class' => 'form-control',
-            'placeholder'=>'Re-enter the password'))!!}
+            'placeholder'=>'Re-enter the password','required'=>'required',
+            'pattern'=>".{6,50}"))!!}
         </div>
         <div class="form-group" style="float:right;">
         {!!Form::submit('Register', array('class' => 'btn btn-primary'))!!}
@@ -33,3 +38,4 @@
         </div>
     </div>
 @stop
+

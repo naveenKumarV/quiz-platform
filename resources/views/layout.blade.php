cd8f782152a864af1d5622f34dcf78b962bbd712
naveenKumarV <naveenkumarvedurupaka@gmail.com>
2015-07-10 15:53:28 +0530

    Include offline jQuery and bootstrap in master  layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,27 +6,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Spider Web Dev Task 3</title>
     <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" />
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    {{--
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    --}}
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <script src="{{ asset('js/jquery-1.11.3.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/menu.css') }}"/>
+    <script src="{{ asset('js/menu.js') }}"></script>
     @yield('header')
     <script>
         $(document).ready(function(){
             $('#myModal').modal();
+            @yield('jquery')
         });
     </script>
 </head>
 <body>
-@if(Session::has('message'))
+
+    @if(Session::has('message'))
     <div id="myModal" class="modal fade" role="dialog">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">{{ Session::get('heading') }}</h4>
+                    <h4 class="modal-title">{{ Session::pull('heading') }}</h4>
                 </div>
                 <div class="modal-body">
-                    <p>{{ Session::get('message') }}.</p>
+                    <p>{{ Session::pull('message') }}.</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -34,7 +45,8 @@
             </div>
         </div>
     </div>
-@endif
+    @endif
+
 @yield('content')
 </body>
 </html>
